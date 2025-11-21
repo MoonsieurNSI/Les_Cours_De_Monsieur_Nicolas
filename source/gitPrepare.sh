acfbdfcfdc089fce27e149e45f902ae4111404d0
MoonsieurNSI <patrice.nicolas7@monlycee.net>
2025-11-21 19:47:14 +0100

    Delete source/gitPrepare.sh

--- source/gitPrepare.sh
+++
@@ -1,9 +0,0 @@
-git config --global user.name "MoonsieuNSI"
-git config --global user.email "patrice.nicolas7@monlycee.net"
-
-git config --global --list
-
-#cle ssh
-ssh-keygen -t ed25519 -C "patrice.nicolas7@monlycee.net"
-
-cat ~/.ssh/id_ed25519.pub
