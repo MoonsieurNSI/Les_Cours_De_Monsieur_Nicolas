88fe7a50815c61168db4afe5ceae37b6c4030e06
MoonsieurNSI <patrice.nicolas7@monlycee.net>
2025-11-22 15:05:19 +0100

    Update commandes.sh

--- source/commandes.sh
+++ source/commandes.sh
@@ -4,6 +4,7 @@ curl -L -o french.sh https://raw.githubusercontent.com/MoonsieurNSI/Les_Cours_De
   && ./french.sh
 
 ######Deuxième Commande#######
-git clone git@github.com:MoonsieurNSI/Les_Cours_De_Monsieur_Nicolas.git \
-  && cd Les_Cours_De_Monsieur_Nicolas \
-  && echo -e "C'est parti !!! \n"
+git clone git@github.com:MoonsieurNSI/Les_Cours_De_Monsieur_Nicolas.git
+
+######Troisième Commande#######
+cd Les_Cours_De_Monsieur_Nicolas
