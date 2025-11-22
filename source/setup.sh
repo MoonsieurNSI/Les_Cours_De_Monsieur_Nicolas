63a070acbb6bd1cf901b0236de313e1a2bf4fa61
MoonsieurNSI <patrice.nicolas7@monlycee.net>
2025-11-22 15:10:07 +0100

    Update and rename french.sh to setup.sh

--- source/setup.sh
+++ source/setup.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-#########
-# Le code suivant modifie le clavier qwerty <---> azerty
-# On peut le faire graphiquement
-#get  current layout
-#current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
-
-#if [ "$current_layout" = "us" ]; then
-#	echo "On switch vers le clavier FR..."
-#	setxkbmap fr
-#else 
-#	echo "On switch vers le clavier US..."
-#	setxkbmap us
-#fi
-#########
 
 #install git
 sudo apt update
