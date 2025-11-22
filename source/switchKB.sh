01a6d7cc71ea71c09bd31a5c52681d4588794b6b
MoonsieurNSI <patrice.nicolas7@monlycee.net>
2025-11-22 15:08:54 +0100

    Create switchKB.sh

---
+++ source/switchKB.sh
@@ -0,0 +1,14 @@
+##########################################################
+# Le code suivant modifie le clavier qwerty <---> azerty #
+##########################################################
+ 
+# get  current layout
+current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
+
+if [ "$current_layout" = "us" ]; then
+	echo "On switch vers le clavier FR..."
+	setxkbmap fr
+else 
+	echo "On switch vers le clavier US..."
+	setxkbmap us
+fi
